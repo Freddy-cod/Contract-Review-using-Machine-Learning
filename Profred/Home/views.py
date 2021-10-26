c272f8d6f1fbde68f1b8fb0395ad8f79b9fbd0a7
Alfred Ndlovu <alfredphronesis@outlook.com>
2021-10-26 13:55:38 +0200

    Initial commit - dev
    added django templates

---
+++ Profred/Home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'Home/index.html')
+
+
+def pricing(request):
+    return render( request,'Home/pricing.html')
+
+
+def about(request):
+    return render( request,'Home/about.html')
+
+
+def blog(request):
+    return render( request,'Home/about.html')
